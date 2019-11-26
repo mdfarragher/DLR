744c64a54d00d37077a396546bedc08ddca37c02
mdfarragher <mdfarragher@gmail.com>
2019-11-26 16:29:10 +0100

    Added LoadBinary methods

--- CNTKUtil/DataUtil.cs
+++ CNTKUtil/DataUtil.cs
@@ -16,6 +16,54 @@ namespace CNTKUtil
     public static class DataUtil
     {
         /// <summary>
+        /// Load the given binary file from disk.
+        /// </summary>
+        /// <param name="filepath">The filename of the file to load.</param>
+        /// <param name="numRows">The number of rows to load.</param>
+        /// <param name="numColumns">The number of columns to load.</param>
+        /// <returns></returns>
+        public static T[][] LoadBinary<T>(
+            string filepath,
+            int numRows,
+            int numColumns)
+        {
+            var size = Marshal.SizeOf(typeof(T)); // warning: unreliable for char!
+            var buffer = new byte[size * numRows * numColumns];
+            using (var reader = new System.IO.BinaryReader(System.IO.File.OpenRead(filepath)))
+            {
+                reader.Read(buffer, 0, buffer.Length);
+            }
+            var dst = new T[numRows][];
+            for (int row = 0; row < dst.Length; row++)
+            {
+                dst[row] = new T[numColumns];
+                Buffer.BlockCopy(buffer, row * numColumns * size, dst[row], 0, numColumns * size);
+            }
+            return dst;
+        }
+
+        /// <summary>
+        /// Load the given binary file from disk.
+        /// </summary>
+        /// <param name="filepath">The filename of the file to load.</param>
+        /// <param name="numRows">The number of rows to load.</param>
+        /// <returns></returns>
+        public static T[] LoadBinary<T>(
+            string filepath,
+            int numRows)
+        {
+            var size = Marshal.SizeOf(typeof(T));
+            var buffer = new byte[size * numRows];
+            using (var reader = new System.IO.BinaryReader(System.IO.File.OpenRead(filepath)))
+            {
+                reader.Read(buffer, 0, buffer.Length);
+            }
+            var dst = new T[numRows];
+            System.Buffer.BlockCopy(buffer, 0, dst, 0, buffer.Length);
+            return dst;
+        }
+
+        /// <summary>
         /// The pretrained VGG16 image classifier.
         /// </summary>
         public static class VGG16
@@ -235,6 +283,7 @@ namespace CNTKUtil
         }
 
     }
+    
 
     /// <summary>
     /// A downloader class to download files from the internet. 
